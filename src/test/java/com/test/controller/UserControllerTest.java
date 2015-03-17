d56b2549731bb27ca151ed4f10948f7d7c4dc40d
zsw <gpgylt@sina.com>
2015-03-17 22:50:23 +0800

    add mock test

--- src/test/java/com/test/controller/UserControllerTest.java
+++ src/test/java/com/test/controller/UserControllerTest.java
@@ -1,9 +1,58 @@
 package com.test.controller;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.ContextHierarchy;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
 /**
  * Created by zsw on 15-3-16.
  */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration(value = "src/main/webapp")
+@ContextHierarchy({
+        @ContextConfiguration(name = "parent", locations = "classpath:spring.xml"),
+        @ContextConfiguration(name = "child", locations = "classpath:spring-mvc.xml"),
+        @ContextConfiguration(name = "child", locations = "classpath:spring-mybatis.xml")
+})
 public class UserControllerTest {
+
+
+
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+
+    @Test
+    public void queryUserTest() throws Exception{
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/user/1"))
+                .andExpect(MockMvcResultMatchers.view().name("user/view"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("user"))
+                .andDo(MockMvcResultHandlers.print())
+                .andReturn();
+
+        Assert.assertNotNull(result.getModelAndView().getModel().get("user"));
+    }
 
 
 }
